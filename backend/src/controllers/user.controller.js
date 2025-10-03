c9816e60a35c094ca22b945e4b28a77fa61e27e4
Sarthak Krishak <sarthakkrishak1234@gmail.com>
2025-10-03 10:25:40 +0530

    register Controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    // Credentials
+    // see if any user exists with those credentials
+    // if not then register it
+})
+
+export {registerUser}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 dotenv.config();
 import { connectDB } from "./db/db.js";
-
+import { app } from "./app.js";
 
 connectDB()
     .then(() => {
