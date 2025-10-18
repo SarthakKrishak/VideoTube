31556b3947d4f9110b5c37ced67688905eab0726
Sarthak Krishak <sarthakkrishak1234@gmail.com>
2025-10-18 13:15:28 +0530

    logout controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,29 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = User.findById(userId);
+        if (!user) {
+            throw new ApiError(401, "User doesnt exists");
+        }
+
+
+    // Generating the refresh and access tokens
+      const accessToken = user.generateAccessToken();
+      const refreshToken = user.generateRefreshToken();
+      
+        // Now storing the refresh token in the database as well
+        user.refreshToken = refreshToken;
+        // We are saving but if we write only save then the mongodb will check for password as well to validate so we are making it as false so now it will not check
+       await user.save({validateBeforeSave:false});
+
+        return { accessToken, refreshToken }; 
+
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating Access and refresh tokenzz")
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     // Destructe the data from the req body
@@ -72,4 +95,57 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    //Steps-
+    // Getting the email and password
+    // Find the user from email
+    // check for the password
+    // access and refresh token generated
+    // send through cookies
+    // send cookie
+
+    const { email, password, username } = req.body;
+    
+    if (!username || !email) {
+        throw new ApiError(400,"Username or email is required")
+    }
+
+    // using mongodb operator for both email and username
+   const user = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (!user) {
+        throw new ApiError(404, "User doesn't exist");
+    }
+
+   const passValid = await user.isPasswordCorrect(password);
+
+    if (!passValid) {
+        throw new ApiError(401, "Invalid Password");
+    }
+
+    // Generating the access and refresh token
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+    
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure:true
+    }
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(200, {
+            user: loggedInUser, accessToken, refreshToken
+        },
+        "User loggedin Successfully"))
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    // Clearing the cookies and refresh token 
+    
+})
+
+export { registerUser, loginUser, logoutUser };
